a912742d9d37a532138c667709fd20d502e1b6fd
MacThings <info@sl-soft.de>
2019-11-04 17:35:32 +0100

    Internal

--- Treeswitcher/ViewController.swift
+++ Treeswitcher/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Sascha Lamprecht on 13/08/2019.
 //  Copyright © 2019 Sascha Lamprecht. All rights reserved.
 //
-//# cd /Users/luigi/Documents/treeswitcher/Treeswitcher/script && shc -r -f scriptori.command && rm *.x.c && cp scriptori.command.x ../bin/treeswitcher && rm scriptori.command.x
+//# cd /Users/luigi/Documents/Xcodetreeswitcher/Treeswitcher/script && shc -r -f scriptori.command && rm *.x.c && cp scriptori.command.x ../bin/treeswitcher && rm scriptori.command.x
 //# cp /Applications/Treeswitcher.app/Contents/Resources/script/script.command /Applications/Treeswitcher.app/Contents/Resources/script/script.command && rm /Applications/Treeswitcher.app/Contents/Resources/script/script.command
 
 
